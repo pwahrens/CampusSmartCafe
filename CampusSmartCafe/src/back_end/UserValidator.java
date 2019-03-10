b972911ae66b5079e6e50f8a2ff9242d8d31b5c6
Zachary Cameron <zacharycameron3@gmail.com>
2019-03-10 14:16:16 -0700

    Fixed User Validator

--- CampusSmartCafe/src/back_end/UserValidator.java
+++ CampusSmartCafe/src/back_end/UserValidator.java
@@ -11,15 +11,24 @@ public class UserValidator {
 	
 	public User login(String userName, String password)
 	{
+		User temp = null;
+		
+		if(checkPassword(userName, password))
+			temp=users.getUser(userName);
+		return temp;
+	}
+	
+	private boolean checkPassword(String userName, String password)
+	{
 		User subject;
 		
 		subject= users.getUser(userName);
-		
+
 		if(subject==null)
-			return subject;
+			return false;
 		
 		if(subject.getPassword().equals(password))
-			return subject;
-		return null;
+			return true;
+		return false;	
 	}
 }
